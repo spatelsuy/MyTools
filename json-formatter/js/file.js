b3f7a3ce96776cdec067a3d4dc783dd358dff267
Sunil Patel <126328736+spatelsuy@users.noreply.github.com>
2026-06-07 18:15:57 -0400

    Update and rename file-handler.js to file.js

--- json-formatter/js/file.js
+++ json-formatter/js/file.js
@@ -1,16 +1,18 @@
-document.getElementById("fileInput").addEventListener("change", function (e) {
+document.getElementById("fileInput").addEventListener("change", e => {
+
     const file = e.target.files[0];
     const reader = new FileReader();
 
     reader.onload = function (event) {
-        document.getElementById("inputArea").value = event.target.result;
+        document.getElementById("input").value = event.target.result;
     };
 
     reader.readAsText(file);
 });
 
 function downloadJSON() {
-    const data = document.getElementById("outputArea").textContent;
+
+    const data = document.getElementById("output").innerText;
 
     const blob = new Blob([data], { type: "application/json" });
     const url = URL.createObjectURL(blob);
